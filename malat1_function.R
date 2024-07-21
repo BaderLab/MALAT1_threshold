841ed04dd51d7b74253dedcbafa163ae89ce4728
Zoe Clarke <41270914+14zac2@users.noreply.github.com>
2024-07-21 16:49:20 -0400

    Update malat1_function.R
    Increased smoothing. Added additional mins and maxes for wonky histograms.

--- malat1_function.R
+++ malat1_function.R
@@ -4,36 +4,52 @@
 # Lower bw if minimum doesn't look totally accurate
 # Change lwd to change thickness of red line
 # Change breaks to change bins of histogram
-# Change chosen_min if you have a very low malat1 peak, lower than 2, that you want to keep
-define_malat1_threshold <- function(counts, bw = 0.1, lwd = 2, breaks = 100, chosen_min = 2) {
+# Increase or decrease chosen_min depending on where your MALAT1 peak is
+# Decrease smooth to reduce smoothing
+# abs_min and rough_max help define minimums and maximums when the histogram is wonky
+define_malat1_threshold <- function(counts, bw = 0.1, lwd = 2, breaks = 100, 
+                                    chosen_min = 1, smooth = 1, abs_min = 0.3,
+                                    rough_max = 2) {
   tryCatch({
     # Calculate the density values
-    density_data <- density(counts, bw = bw)
+    density_data <- density(counts, bw = bw,
+                           from = min(counts),
+                            to = max(counts))
     # Fit a smooth line to the data
-    fit <- smooth.spline(density_data$x, density_data$y, spar = 0.5)
+    fit <- smooth.spline(density_data$x, density_data$y, spar = smooth)
     # Predict the first derivative
     first_derivative <- predict(fit, density_data$x, deriv = 1)
-    
-    # Find the local minima
-    local_minima <- density_data$x[which(diff(sign(first_derivative$y)) == 2)]
-    # Plot the density with the local minima
-    plot(density_data, main = "Density Plot with Local Minima")
-    lines(fit, col = "blue")
-    points(local_minima, predict(fit, local_minima)$y, col = "red", pch = 19)
-    
+
     # Find the local maxima
     local_maxima <- density_data$x[which(diff(sign(first_derivative$y)) == -2)]
+    if(length(local_maxima) == 0) {
+      # Find x-val closest to rough_max
+      local_maxima <- density_data$x[which(abs(density_data$x - rough_max) == min(abs(density_data$x - rough_max)))]
+    }
     # Plot the density with the local maxima
     plot(density_data, main = "Density Plot with Local Maxima")
     lines(fit, col = "blue")
     points(local_maxima, predict(fit, local_maxima)$y, col = "red", pch = 19)
     
-    # Find biggest local maximum greater than desired value (default 2)
+    # Find the local minima
+    local_minima <- density_data$x[which(diff(sign(first_derivative$y)) == 2)]
+    if(length(local_minima) == 0) {
+      local_minima <- abs_min
+    }
+    # Plot the density with the local minima
+    plot(density_data, main = "Density Plot with Local Minima")
+    lines(fit, col = "blue")
+    points(local_minima, predict(fit, local_minima)$y, col = "red", pch = 19)
+    
+    # Find biggest local maximum greater than desired value (default 1)
     biggest_y <- max(density_data$y[density_data$x %in% local_maxima[local_maxima > chosen_min]])
     maxi <- density_data$x[density_data$y == biggest_y]
     
     # Find local minimum closest to the left of that
     local_minima <- local_minima[local_minima < maxi]
+    if(length(local_minima) < abs_min) {
+      local_minima <- abs_min
+    }
     mini <- local_minima[(maxi - local_minima) == min(maxi - local_minima)]
     
     # Calculate delta to get range of values to isolate for quadratic calculation
@@ -67,6 +83,9 @@ define_malat1_threshold <- function(counts, bw = 0.1, lwd = 2, breaks = 100, cho
     discriminant <- b^2 - 4 * a * c
     # Grab first intercept
     x_intercept1 <- (-b + sqrt(discriminant)) / (2 * a)
+    if(x_intercept1 < 0) {
+      x_intercept1 <- abs_min
+    }
     
     # Plot histogram with threshold
     hist(counts, breaks = breaks,
